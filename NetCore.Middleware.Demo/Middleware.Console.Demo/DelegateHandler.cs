08f49e7f98e5e9242800cdfb2c000de073b0723f
Cang Yi Xie <cangyi.xie@ncsi.com.cn>
2023-04-18 20:36:33 +0800

    added console.demo

---
+++ NetCore.Middleware.Demo/Middleware.Console.Demo/DelegateHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware.Console.Demo;
+
+public delegate void NextChainDelegate(string data);
+
+public delegate NextChainDelegate ChainDelegate(NextChainDelegate next);
+
+public static class DelegateHandler
+{
+    public static ChainDelegate CombineHandler(ChainDelegate[] handlers)
+    {
+        return next =>
+        {
+            foreach (var handler in handlers.Reverse())
+            {
+                next = handler(next);
+            }
+            return next;
+        };
+    }
+}
---
+++ NetCore.Middleware.Demo/Middleware.Console.Demo/Program.cs
@@ -0,0 +1,77 @@
+﻿using Middleware.Console.Demo;
+
+class Program
+{
+
+    static ChainDelegate handler1 = next => (
+    str =>
+    {
+        Console.WriteLine("handler1:" + str);
+        next(str);
+    });
+
+    static ChainDelegate handler2 = next => (
+    str =>
+    {
+        Console.WriteLine("handler2:" + str);
+        next(str);
+    });
+
+    static ChainDelegate handler3 = next => (
+    str =>
+    {
+        Console.WriteLine("handler3:" + str);
+        next(str);
+    });
+
+    private static void Main(string[] args)
+    {
+
+
+        //NextDelegateTest();
+
+        var del = DelegateHandler.CombineHandler(new ChainDelegate[2] { handler1, handler2 });
+
+        NextChainDelegate next = i => Console.WriteLine("test1:" + i);
+        next = del(next);
+        next = handler3(next);
+        next("hello");
+
+        Console.ReadKey();
+    }
+
+
+    private static void NextDelegateTest()
+    {
+        NextChainDelegate next = i => Console.WriteLine("test1:" + i);
+        next = handler1(next);
+        next = handler2(next);
+        next("hello");
+    }
+
+
+    private static void ActionTest()
+    {
+        Action<int> action;
+        action = a => Console.WriteLine("input:" + a);
+        action(1);
+        action.Invoke(2);
+    }
+
+    private static void FunctionTest()
+    {
+        Func<int, int> fun1;
+        fun1 = a => { return a + 1; };
+        var result = fun1.Invoke(1);
+        Console.WriteLine("input:" + result);
+    }
+
+    public delegate string PrintDelegate(int x);
+
+    private static void DelegateTest()
+    {
+        PrintDelegate del = i => "i=" + i;
+        var str = del(1);
+    }
+
+}
