ce26ac693d8daf8612734443cfe61a17a9c20453
Cang Yi Xie <cangyi.xie@ncsi.com.cn>
2023-10-12 15:18:51 +0800

    upload Layout,Pagination Select demo

---
+++ Ant-Table-Demo/Ant-Pagination-Demo/Program.cs
@@ -0,0 +1,12 @@
+using Ant_Pagination_Demo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddAntDesign();    //ant design
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
