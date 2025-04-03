b0f82fce80f0771cfc8d0ff28173f71d5a59c6f4
Cang Yi Xie <cangyi.xie@ncsi.com.cn>
2025-04-03 15:16:27 +0800

    added Menu-Demo Example4

---
+++ Ant-Table-Demo/Ant-Menu-Demo/Pages/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace Ant_Menu_Demo.Pages;
+
+public class Node
+{
+    public ICollection<Node> ChildNodes { get; set; } = [];
+    public string Title { get; set; }
+    public string Image { get; set; }
+}
