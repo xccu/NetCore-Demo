4f0f481489e77a592c41dbc3757fc628a05ff2d5
Cang Yi Xie <cangyi.xie@ncsi.com.cn>
2026-01-07 11:13:51 +0800

    added lazyLoad Test

---
+++ Blazor.Demo/10.0/GrantImaharaRobotControls/HandGesture.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GrantImaharaRobotControls;
+
+public static class HandGesture
+{
+    public static bool ThumbUp(Axis axis, ILogger logger)
+    {
+        logger.LogInformation("Thumb up gesture. Axis: {Axis}", axis);
+
+        // Code to make robot perform gesture
+
+        return true;
+    }
+}
+
+public enum Axis { Left, Right }
